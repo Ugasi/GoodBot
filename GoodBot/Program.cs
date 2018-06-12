b24d9567a7c5d7528088297425b3ada66a093281
Juha Palmu <juhamikaelpalmu@gmail.com>
2018-06-12 12:56:15 +0300

    Add project files.

---
+++ GoodBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoodBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
