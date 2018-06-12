e29846a51efcf53de5e02aa68f1e7b1e41a17134
Juha Palmu <juhamikaelpalmu@gmail.com>
2018-06-12 16:47:53 +0300

    Add basic login for bot

--- GoodBot/Program.cs
+++ GoodBot/Program.cs
@@ -1,12 +1,78 @@
-﻿using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
 
 namespace GoodBot
 {
     class Program
     {
+        private static IConfigurationRoot Configuration;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InitConfiguration();
+            new Program().RunBot().GetAwaiter().GetResult();
+        }
+
+        private static void InitConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+            Configuration = builder.Build();
+        }
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        public async Task RunBot()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            string token = Configuration["DiscordToken"];
+
+            client.Log += Log;
+
+            await RegisterCommands();
+            await client.LoginAsync(TokenType.Bot, token: token);
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommands()
+        {
+            client.MessageReceived += HandleCommand;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommand(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            if (message is null || message.Author.IsBot) return;
+            int argPos = 0;
+            if(message.HasStringPrefix("tnt!", ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos)) {
+                var context = new SocketCommandContext(client, message);
+                var result = await commands.ExecuteAsync(context, argPos, services);
+                if (!result.IsSuccess) {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
         }
     }
 }
